197d768a5f25a34d3018fc824d6334bdabb3db3f
MJ <mj@MJs-MacBook-Air.fios-router.home>
2019-11-18 21:32:42 -0500

    react Jest working

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+configure({ adapter: new Adapter() })
+
+describe('<App />', () => {
+  describe('Snapshots', () => {
+    it('should render the login form if no user is present', () => {
+      const rendered = shallow(<App />)
+      expect(rendered.debug()).toMatchSnapshot()
+    })
+  })
 })
