d56205f7eec23d243fe80369064b02c18c53fd2f
MJ <mj@MJs-MacBook-Air.fios-router.home>
2019-11-18 21:04:01 -0500

    react web login working

--- web/src/App.js
+++ web/src/App.js
@@ -1,30 +1,65 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from 'react'
 
-function App () {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-}
+    import api from './Api'
 
-export default App
+    import './App.css'
+
+    class App extends Component {
+      state = {
+        user: null,
+        email: '',
+        password: '',
+      }
+
+      loginUser = async (e) => {
+        e.preventDefault()
+
+        const { email, password } = this.state
+
+        const { data: token } = await api.loginUser(email, password)
+
+        const { data: user } = await api.getUser(token)
+
+        this.setState({ user })
+      }
+
+      inputChange = e => this.setState({ [e.target.id]: e.target.value })
+
+      renderLoginForm = () => {
+        const { email, password } = this.state
+
+        return (
+          <form onSubmit={this.loginUser}>
+            <h1>An Awesome Form on React</h1>
+            <label htmlFor="email">
+              <span>Email:</span>
+              <input onChange={this.inputChange} value={email} id="email" name="email" type="email" />
+            </label>
+            <br />
+            <label htmlFor="password">
+              <span>Password:</span>
+              <input onChange={this.inputChange} value={password} id="password" name="password" type="password" />
+            </label>
+            <br />
+            <button type="submit">Login</button>
+          </form>
+        )
+      }
+
+      render () {
+        const { user } = this.state
+
+        return (
+          <div className="App">
+            {user ? (
+              <>
+                <div>{`Welcome ${user.email}!`}</div>
+                <div>{`Your name is ${user.firstname} ${user.lastname}`}</div>
+              </>
+            ) : this.renderLoginForm()}
+          </div>
+        )
+      }
+    }
+
+    export default App
